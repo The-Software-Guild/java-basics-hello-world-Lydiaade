3a4e1e068c3a9e956caa8ac9cb91216d05e4bc4a
Lydia <lydiaadejumo@outlook.com>
2020-06-03 14:58:10 +0100

    Hello World in Netbeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author lydiaadejumo
+ */
+package alydiaade.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, my name is Lydia");
+    }
+}
