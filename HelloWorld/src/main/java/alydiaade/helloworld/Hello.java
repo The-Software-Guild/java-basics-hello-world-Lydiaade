2fab9cd14ada5570d98c949e2edcd455c54552bb
Lydia <lydiaadejumo@outlook.com>
2020-06-04 19:49:04 +0100

    Debugger Check

--- HelloWorld/src/main/java/alydiaade/helloworld/Hello.java
+++ HelloWorld/src/main/java/alydiaade/helloworld/Hello.java
@@ -8,5 +8,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, my name is Lydia");
+        System.out.println("Just checking the debugger!!");
     }
 }
